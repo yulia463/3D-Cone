c54734cde33288cdf550e620ef88ab0c8bd1d3ff
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-09-04 12:14:53 +0200

    feat : fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,5 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
 import * as THREE from 'three';
+import {useEffect} from "react";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -40,7 +15,7 @@ scene.add(cube);
 
 camera.position.z = 5;
 
-const animate = () => {
+export const animate = () => {
   requestAnimationFrame(animate);
 
   cube.rotation.x += 0.01;
@@ -49,4 +24,11 @@ const animate = () => {
   renderer.render(scene, camera);
 };
 
-animate();
+
+ const App = () => {
+  useEffect(() => {
+    animate()
+  }, [])
+
+}
+export default App
